250717d83396a7cc94c8e89414714ecaefe37171
Olivier Messelink <oliviermesselink@gmail.com>
2024-01-05 14:02:07 +0100

    Added props for config, events and date selector

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="div_wrapper">
-    <calendar-vue></calendar-vue>
+    <calendar-vue :date_selector="date" :config="cal_config"></calendar-vue>
   </div>
 </template>
 
 <script>
 import CalendarVue from './components/CalendarComp.vue';
 export default {
+  data() {
+    return {
+      date: new Date,
+      cal_config: {
+        week_start: 'monday'
+      }
+    }
+  },
   components: {
     CalendarVue
   }
@@ -15,7 +23,7 @@ export default {
 
 <style>
 .div_wrapper {
-  height: 600px;
-  width: 450px;
+  height: 80vh;
+  width: 100%;
 }
 </style>
--- src/components/CalendarComp.vue
+++ src/components/CalendarComp.vue
@@ -1,47 +1,69 @@
-<template lang="eng">
+<template>
     <div class="cal_wrapper">
-        <div class="cal_cell" v-for="day in renderMonthGrid" :key="day" :class="day.classes">
-            <p>{{day.date}}</p>
+        <div class="cal_cell" v-for="day in generateMonthGrid" :key="day" :class="day.classes"
+            @click="createEvent(day.full_date)">
+            <p>{{ day.date }}</p>
+            <div v-for="event in day.events" :key="event" class="event" :class="event.classes">{{ event.description }}</div>
         </div>
     </div>
 </template>
+
 <script>
-import { getDate, setDate, previousMonday, previousSunday, addDays, isPast, isToday, isFuture, isThisMonth } from "date-fns"
+import { getDate, setDate, previousMonday, previousSunday, addDays, isPast, isToday, isFuture, isThisMonth, isSameDay, isMonday, isSunday } from "date-fns"
+
 
 export default {
-    data() {
-        return {
-            date_selector: new Date,
-            config: {
-                week_start: 'monday',
-            }
-
+    props: {
+        date_selector: {
+            required: true
+        },
+        config: {
+            type: Object,
+            required: true,
+        },
+        events: {
+            type: Array,
+            required: false
         }
     },
+    methods: {
+
+    },
     computed: {
-        getToday() {
-            return getDate(this.date_selector)
-        },
-        renderMonthGrid() {
+        // Generates an array of 35 day-cells starting at the last monday/sunday relative to the 1st of the month
+        generateMonthGrid() {
             let current_date = this.date_selector;
             let first_month_day = setDate(current_date, 1);
             let grid_start_day = null;
             let month_grid = [];
 
+            // Getting first day of week from config and then getting the previous monday/sunday to start the grid
             if (this.config.week_start === 'monday') {
-                grid_start_day = previousMonday(first_month_day);
+                if (!isMonday(first_month_day)) {
+                    grid_start_day = previousMonday(first_month_day);
+                } else {
+                    grid_start_day = first_month_day
+                }
             } else if (this.config.week_start === 'sunday') {
-                grid_start_day = previousSunday(first_month_day);
+                if (!isSunday(first_month_day)) {
+                    grid_start_day = previousSunday(first_month_day);
+                } else {
+                    grid_start_day = first_month_day
+                }
             } else {
-                return "unknow argument: " + this.config.week_start + " , expected : 'monday'/'sunday' "
+                console.log("unknow argument: " + this.config.week_start + " , expected : 'monday' or 'sunday' ")
             }
 
+            // Looping trough the cells and incrementing the date
             for (let i = 0; i < 35; i++) {
                 let incr_date = addDays(grid_start_day, i);
                 let day = {};
 
                 day.full_date = incr_date;
                 day.date = getDate(incr_date);
+                day.events = [];
+
+                // Adds classes to the cells, to differentiate between past, present, last month etc..
                 day.classes = [];
 
                 if (isPast(incr_date) && !isToday(incr_date)) { day.classes.push('past') }
@@ -49,6 +71,13 @@ export default {
                 if (isFuture(incr_date)) { day.classes.push('future') }
                 if (!isThisMonth(incr_date)) { day.classes.push('other_month') }
 
+
+                if (this.events) {
+                    this.events.forEach(event => {
+                        if (isSameDay(event.date, day.full_date)) { day.events.push(event) }
+                    })
+
+                }
 
                 month_grid.push(day);
             }
@@ -59,46 +88,56 @@ export default {
 
 }
 </script>
+
 <style scoped lang="css">
 .cal_wrapper {
     height: 100%;
     width: 100%;
     display: grid;
-    gap: 2px;
     grid-template-columns: repeat(7, 1fr);
     grid-template-rows: repeat(5, 1fr);
     font-family: sans-serif;
+    border: 1px solid rgb(231, 231, 231);
+    border-width: 0 0 1px 1px;
 }
 
 .cal_cell {
     width: 100%;
     height: 100%;
-    border: 1px solid rgb(231, 231, 231);
-    padding: 8px;
+    border-top: 1px solid rgb(231, 231, 231);
+    border-right: 1px solid rgb(231, 231, 231);
+    padding: 12px 4px;
     box-sizing: border-box;
     cursor: pointer;
+    text-align: center;
 }
 
 .cal_cell:hover {
     transition: ease-in-out 150ms;
-    color: blue;
-    border-color: blue;
+    background-color: rgba(0, 0, 255, 0.08);
     opacity: 1;
 }
 
 .today {
-    border: blue 2px solid;
+    border: blue 1px solid;
     background-color: rgba(0, 0, 255, 0.08);
     box-sizing: border-box;
     opacity: 1;
 }
 
-.past {
-    opacity: .25;
+.today p {
+    font-weight: bold;
 }
 
-.other_month {
-    background-color: rgb(231, 231, 231);
+.past {
+    opacity: .4;
+}
 
+.event {
+    font-size: 12px;
+    background-color: blue;
+    padding: 4px 1px;
+    color: white;
+    margin-top: 8px;
 }
 </style>
